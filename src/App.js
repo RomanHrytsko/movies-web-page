5c4dddd6861b33d79364d46d6cbec614d25e3cb6
RomanHrytsko <romangritsko@gmail.com>
2021-01-15 16:54:58 +0200

    commit before restart

--- src/App.js
+++ src/App.js
@@ -3,29 +3,30 @@ import logo from './logo.svg';
 import './App.css';
 import {movieService} from "./services";
 
-function App() {
-useEffect(()=>{
-  movieService.getMovies().then(console.log)
-},[])
-  return (
+const PageLayout = ({children}) => {
+    return (
+        <div className={'main-wrapper'}>
+            <header>Header data</header>
+                <main>
+                    {children}
+                </main>
+            <footer>Footer data</footer>
 
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        </div>
+    )
+}
+
+
+function App() {
+    useEffect(() => {
+        movieService.getMovies().then(console.log)
+    }, [])
+    return (
+
+        <PageLayout>
+            <div>hello</div>
+        </PageLayout>
+    );
 }
 
 export default App;
